eae300b87bd6c938bc77c09c42b73669a54a98d6
Alifmslm <alifmslm01@gmail.com>
2023-06-23 14:38:07 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// const searchButton = document.querySelector(".search--button");
+// const headRight = document.querySelector(".head--right");
+// const headLeft = document.querySelector(".head--left");
+// const titleWeb = document.querySelector(".title--web");
+// const searchText = document.querySelector(".search--text");
+
+// const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+// const whenClicked = searchClicked();
+
+// function searchClicked() {
+//     searchButton.style.justifyContent = "start";
+//     headLeft.style.width = "100%";
+//     titleWeb.style.display = "none";
+//     headRight.style.display = "none";
+//     searchText.style.display = "block";
+// }
+
+// const defaultSearch = searchDefault();
+
+// function searchDefault() {
+//     headLeft.style.width = "auto";
+//     titleWeb.style.display = "block";
+//     headRight.style.display = "block";
+//     searchText.style.display = "none";
+// }
+
+// if (mediaQuery === true) {
+//     searchButton.addEventListener('click', () => {
+//         whenClicked();
+//     })
+// } else {
+//     searchDefault();
+// }
